30139072de2684252965f5d85c553c6b7a99f778
David Wong <89150042+dWong991@users.noreply.github.com>
2023-10-04 15:53:54 -0700

    Update IntroductiontoPandas.py

--- IntroductiontoPandas.py
+++ IntroductiontoPandas.py
@@ -249,3 +249,20 @@ import pandas as pd
 def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     return pd.concat([df1, df2])
 
+'''
+2889. Reshape Data: Pivot
+DataFrame weather
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| city        | object |
+| month       | object |
+| temperature | int    |
++-------------+--------+
+Write a solution to pivot the data so that each row represents temperatures for a specific month, and each city is a separate column.
+'''
+
+import pandas as pd
+
+def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+    return weather.pivot(index="month", columns="city", values='temperature')
