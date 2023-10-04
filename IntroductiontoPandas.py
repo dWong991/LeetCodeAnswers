946d403d49fafd6d7bdba83b00c94eac19dd9b7c
David Wong <89150042+dWong991@users.noreply.github.com>
2023-10-04 15:26:20 -0700

    Update IntroductiontoPandas.py

--- IntroductiontoPandas.py
+++ IntroductiontoPandas.py
@@ -152,3 +152,29 @@ import pandas as pd
 def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
     employees["salary"] = employees["salary"] * 2
     return employees
+
+'''
+2885. Rename Columns
+DataFrame students
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| id          | int    |
+| first       | object |
+| last        | object |
+| age         | int    |
++-------------+--------+
+Write a solution to rename the columns as follows:
+
+id to student_id
+first to first_name
+last to last_name
+age to age_in_years
+
+'''
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students = students.rename(columns={"id" : "student_id", "first" : "first_name", "last" : "last_name", "age" : "age_in_years"})
+    return students
+
