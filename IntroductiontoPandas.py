81d174b3b6f5cbd475470d3600f52f9d80edb802
David Wong <89150042+dWong991@users.noreply.github.com>
2023-10-04 15:20:41 -0700

    Update IntroductiontoPandas.py

--- IntroductiontoPandas.py
+++ IntroductiontoPandas.py
@@ -133,3 +133,22 @@ import pandas as pd
 def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
     students = students[students['name'].notnull()]
     return students
+'''
+2884. Modify Columns
+DataFrame employees
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| name        | object |
+| salary      | int    |
++-------------+--------+
+A company intends to give its employees a pay rise.
+
+Write a solution to modify the salary column by multiplying each salary by 2.
+'''
+
+import pandas as pd
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees["salary"] = employees["salary"] * 2
+    return employees
