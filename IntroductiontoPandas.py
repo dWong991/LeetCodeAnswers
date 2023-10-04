02d09ad825dd92977e0e9165794b93b882d2589d
David Wong <89150042+dWong991@users.noreply.github.com>
2023-10-04 15:42:45 -0700

    Update IntroductiontoPandas.py

--- IntroductiontoPandas.py
+++ IntroductiontoPandas.py
@@ -223,3 +223,29 @@ def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
     return products
 
 '''
+2888. Reshape Data: Concatenate
+DataFrame df1
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| student_id  | int    |
+| name        | object |
+| age         | int    |
++-------------+--------+
+
+DataFrame df2
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| student_id  | int    |
+| name        | object |
+| age         | int    |
++-------------+--------+
+
+Write a solution to concatenate these two DataFrames vertically into one DataFrame.
+'''
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2])
+
