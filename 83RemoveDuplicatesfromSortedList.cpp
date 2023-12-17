3b04288c0929207d808e2d7be68c6c14e20191d0
David Wong <89150042+dWong991@users.noreply.github.com>
2023-12-17 11:11:44 -0800

    Create 83RemoveDuplicatesfromSortedList.cpp

---
+++ 83RemoveDuplicatesfromSortedList.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* currNode = head;
+        while(currNode != nullptr && currNode->next != nullptr){
+            if(currNode->val == currNode->next->val){
+                currNode->next = currNode->next->next;
+            }
+            else{
+                currNode = currNode->next;
+            }
+        }
+        return head;
+    }
+};
